62049b04fe457cd13bb08dc932b9f860cec4e28c
MDMOQADDAS <69861558+MDMOQADDAS@users.noreply.github.com>
2021-05-11 19:31:38 +0530

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,20 @@
+import os
+
+
+#main1
+os.system("ansible-playbook main1.yml")
+
+#main2
+
+os.system("ansible-playbook main2.yml")
+
+os.system("ansible-playbook main3.yml")
+
+os.system("ansible-playbook main4.yml")
+os.system("ansible-playbook main5.yml")
+os.system("ansible-playbook main6.yml")
+os.system("ansible-playbook main7.yml")
+os.system("ansible-playbook main8.yml")
+os.system("ansible-playbook main9.yml")
+
+
